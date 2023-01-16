a782a79cb2640a2e3627e3fff879ddc15ef3fd79
Dismas <dimich14@gmail.com>
2023-01-16 15:21:06 +0300

    first commit

---
+++ src/main/java/com/dismas/gitgradle/GitgradleApplication.java
@@ -0,0 +1,13 @@
+package com.dismas.gitgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitgradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitgradleApplication.class, args);
+	}
+
+}
